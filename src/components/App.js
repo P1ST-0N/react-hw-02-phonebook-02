d0b7d56ed5b04e3c220132c7da1b25df9fba3a67
P1ST-0N <luckylionia@gmail.com>
2023-04-11 14:47:24 +0300

    create new react app, added deps

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import nextId from "react-id-generator";
+
+export class App extends Component {
+
+  render() {
+
+    return (
+      <>
+
+      </>
+    )
+  };
+}
+
+export default App;
